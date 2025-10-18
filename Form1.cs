ebfb88ef40f940ac2b026f6976d89c834a92dde2
Equa <TheSuspect9980@gmail.com>
2025-10-18 17:43:06 +0300

    0.3.2 printers test

--- Form1.cs
+++ Form1.cs
@@ -4,11 +4,13 @@ using System.Media;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using System.IO;
+using System.Drawing.Printing;
 
 namespace TicketApp
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
--- Program.cs
+++ Program.cs
@@ -5,6 +5,29 @@ namespace TicketApp
 {
     static class Program
     {
+
+        // public MainForm()
+        // {
+        //     this.Text = "Printer Test";
+        //     this.Width = 300;
+        //     this.Height = 150;
+
+        //     printerCombo = new ComboBox();
+        //     printerCombo.Left = 20;
+        //     printerCombo.Top = 20;
+        //     printerCombo.Width = 240;
+
+        //     foreach (string printer in PrinterSettings.InstalledPrinters)
+        //     {
+        //         printerCombo.Items.Add(printer);
+        //     }
+
+        //     if (printerCombo.Items.Count > 0)
+        //         printerCombo.SelectedIndex = 0;
+
+        //     this.Controls.Add(printerCombo);
+        // }
+
         [STAThread]
         static void Main()
         {
