df61ac195f04f0eec994a3753f8b3a6c7e82072e
Equa <TheSuspect9980@gmail.com>
2025-10-18 18:52:18 +0300

    0.3.3 - dev start

---
+++ Scripts/Form1.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Windows.Forms;
+using System.Media;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+using System.Drawing.Printing;
+
+namespace TicketApp
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
