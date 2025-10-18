bc7182c825ced84f84d737906be44cccfedf76c5
Equa <TheSuspect9980@gmail.com>
2025-10-18 20:49:59 +0300

    0.3.5 - full screen window test

--- Scripts/Forms/StudentForm.cs
+++ Scripts/Forms/StudentForm.cs
@@ -1,12 +1,48 @@
+using System;
 using System.Windows.Forms;
 
 namespace TicketApp
 {
     public partial class StudentForm : Form
     {
+        private bool isFullScreen = false;
+        private FormBorderStyle previousFormBorderStyle;
+        private System.Drawing.Rectangle previousBounds;
+
         public StudentForm()
         {
             InitializeComponent();
+            this.KeyPreview = true; // чтобы ловить клавиши на форме
+            this.KeyDown += StudentForm_KeyDown;
+        }
+
+        private void StudentForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F11)
+            {
+                ToggleFullScreen();
+            }
+        }
+
+        private void ToggleFullScreen()
+        {
+            if (!isFullScreen)
+            {
+                previousFormBorderStyle = this.FormBorderStyle;
+                previousBounds = this.Bounds;
+
+                this.FormBorderStyle = FormBorderStyle.None;
+                this.WindowState = FormWindowState.Maximized;
+
+                isFullScreen = true;
+            }
+            else
+            {
+                this.FormBorderStyle = previousFormBorderStyle;
+                this.Bounds = previousBounds;
+
+                isFullScreen = false;
+            }
         }
     }
 }
